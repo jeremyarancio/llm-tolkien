0e76a9e30df3e85cc86905a790cc5b215a75f439
JeremyArancio <jeremyarancio@gmail.com>
2023-05-10 20:16:51 +0200

    Handle memory issue with batches

--- llm/config.py
+++ llm/config.py
@@ -9,4 +9,5 @@ header_height = 60  # Main text distance from the top of the page: to remove hea
 footer_height = 540 # Remove footer
 start_page = 45
 end_page = 1055
-extraction_path = REPO_DIR / "llm/data/extracted_text.json"
+extraction_path = REPO_DIR / "llm/data/extracted_text.jsonl"
+batch_size = 300
--- llm/prepare_dataset.py
+++ llm/prepare_dataset.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Generator, List
 import requests
 import io
 import logging
@@ -17,38 +17,50 @@ logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
 
-def get_all_cropped_text(url: str) -> None:
-    cropped_pages: Dict[int, str] = defaultdict()
+def extract_pages_as_batches_from_url(url: str, batch_size: int) -> Generator[List[Page], None, None]:
+    LOGGER.info(f'Start extracting pages from {url}')
     response = requests.get(url)
     content = io.BytesIO(response.content)
-    pdf = pdfplumber.open(content)
-    for page in pdf.pages:
-        if page.page_number >= config.start_page and page.page_number <= config.end_page:
-            timestamp = time.time()
-            cropped_page = extract_cropped_page(
-                page=page,
-                header_height=config.header_height,
-                footer_height=config.footer_height
-            )
-            cropped_pages[page.page_number] = cropped_page.extract_text()
-            LOGGER.info(f'It took {time.time() - timestamp} seconds to extract page {page.page_number}.')
-    LOGGER.info(f"{len(cropped_pages)} pages were extracted from the pdf.")
-    to_json(cropped_pages=cropped_pages, path=config.extraction_path)
+    pages = pdfplumber.open(content).pages
+    LOGGER.info(f'Finished extracting pages from {url}')
+    return batch_loader(pages, batch_size=batch_size)
 
 
-def extract_cropped_page(page: Page, header_height: int, footer_height: int):
+def batch_loader(pages: List, batch_size: int) -> Generator[List[Page], None, None]:
+    for i in range(0, len(pages), batch_size):
+        yield pages[i:i + batch_size]
+
+
+def extract(url: str, batch_size: int) -> None:
+    """Extract text over selected pages
+
+    Args:
+        url (str): url of the pdf
+        batch_size (int): number of pages to extract at once to avoid memory issues
+    """
+    batches = extract_pages_as_batches_from_url(url=url, batch_size=batch_size)
+    for batch in batches:
+        # We append text to the existing file with "a" mode (append)
+        with open(config.extraction_path, 'a') as f:
+            for page in batch:
+                if page.page_number >= config.start_page and page.page_number <= config.end_page:
+                    timestamp = time.time()
+                    cropped_page = extract_cropped_page(
+                        page=page,
+                        header_height=config.header_height,
+                        footer_height=config.footer_height
+                    )
+                    dict_page = {page.page_number: cropped_page}
+                    json.dump(dict_page, f)
+                    f.write('\n')
+                    LOGGER.info(f'It took {time.time() - timestamp} seconds to extract page {page.page_number}.')
+
+
+def extract_cropped_page(page: Page, header_height: int, footer_height: int) -> str:
     bbox = (0, header_height, page.width, footer_height) # Top-left corner, bottom-right corner
     cropped_page = page.crop(bbox=bbox)
-    return cropped_page
-
-
-def to_json(cropped_pages: Dict[int, str], path: Path):
-    with open(path, 'w') as f:
-        json.dump(cropped_pages, f, indent=4)
+    return cropped_page.extract_text()
 
 
 if __name__ == "__main__":
-    cropped_pages = get_all_cropped_text(url=config.url)
-    for page, text in cropped_pages.items():
-        print(text)
-        break
+    extract(url=config.url, batch_size=config.batch_size)
